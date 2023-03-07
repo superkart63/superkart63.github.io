528944f7bb3fcc7cfa4c488ef7cccd3244f80f7c
superkart63 <superkart63@web.de>
2023-03-07 08:22:49 +0100

    add counter alert

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ let counter = 0;
 function count() {
   counter++;
   document.querySelector('h1').innerHTML = counter;
+  if (counter % 10 === 0){
+    alert(`Count is now ${counter}`);
+  }
 }
 
 
